ebbe9766cebb1b7fecdd5eb6f35f502da62584db
kaif8077 <149231751+kaif8077@users.noreply.github.com>
2025-11-23 23:16:13 +0530

    Refactor App component and update relatives list

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  return (
-    <div id="main">
-               {/* Do not remove the main div */}
-    </div>
-  )
-}
 
-export default App
+  const relatives = [
+    { id: "relativeListItem1", name: "Aunt" },
+    { id: "relativeListItem2", name: "Uncle" },
+    { id: "relativeListItem3", name: "Cousin" },
+    { id: "relativeListItem4", name: "Grandma" },
+    { id: "relativeListItem5", name: "Grandpa" },
+  ];
+
+  return (
+    <div>
+      <h2>Relatives to Visit this Diwali</h2>
+      <ol key="relativeList">
+        {relatives.map((relative) => (
+          <li key={relative.id}>{relative.name}</li>
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+export default App;
